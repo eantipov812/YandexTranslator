dbed3ec809922eaeefc4af112a763a619b7c8e0e
eantipov812 <antipovem@gmail.com>
2018-12-28 11:06:24 -0500

    Initial Commit

---
+++ YandexTranslator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YandexTranslator
+//
+//  Created by Egor Antipov on 12/28/18.
+//  Copyright © 2018 Egor Antipov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
